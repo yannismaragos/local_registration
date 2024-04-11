fe569b52171ee14fa9fbd733cb6a557847acd67d
Yannis Maragos <maragos.y@wideservices.gr>
2024-04-11 11:08:21 +0300

    feat: hide body when redirecting with JavaScript

--- classes/helper/Router.php
+++ classes/helper/Router.php
@@ -76,7 +76,10 @@ class Router {
 
             // Redirect via JavaScript.
             $url = $url->out(false);
-            echo '<script>document.location.href=' . json_encode($url) . ";</script>\n";
+            echo '<script>
+            document.querySelector("body").style.display = "none";
+            document.location.href=' . json_encode($url) . ";
+            </script>\n";
         } else {
             redirect($url, $message, $delay, $messagetype);
         }
